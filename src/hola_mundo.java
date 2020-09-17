8b8d06d437ab06aefeb10b5034dc99b24ce047e6
CONTA-47 <CONTA-47@DESKTOP-RQ7SNBR>
2020-09-17 17:25:53 -0500

    primer ejercicio hola mundo

---
+++ src/hola_mundo.java
@@ -0,0 +1,17 @@
+
+public class hola_mundo {
+
+	public static void main(String[] args) {
+		// primer hola mundo en java
+		
+		System.out.println("hola mundo");
+		
+		System.out.println("mi nombre es efren");
+		
+		System.out.println("soy el elegido de este programa");
+		
+		System.out.println("bueno adios");
+		
+	}
+
+}
