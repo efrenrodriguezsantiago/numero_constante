bccbbc65ad0aeb0aeefeffda63985282c928954f
CONTA-47 <CONTA-47@DESKTOP-RQ7SNBR>
2020-09-17 17:33:20 -0500

    numero no primitivo

---
+++ src/no_primitivo_cadena.java
@@ -0,0 +1,18 @@
+
+public class no_primitivo_cadena {
+
+	public static void main(String[] args) {
+		
+		Integer numero = null;
+		
+		System.out.println("es un dato no primitivo "+numero);
+		
+		//--------------------------------cadenas--------------------
+		
+		String palabra = "hola que tal";
+		
+		System.out.println("es una cadena de caracteres "+palabra);
+
+	}
+
+}
