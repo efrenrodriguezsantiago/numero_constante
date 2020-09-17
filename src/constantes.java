3c3cc193c3d112cf63355ace4c399ab9c77f6ccb
CONTA-47 <CONTA-47@DESKTOP-RQ7SNBR>
2020-09-17 17:24:01 -0500

    estees un numero constante entrada y salida de datos con system.out etc...

---
+++ src/constantes.java
@@ -0,0 +1,17 @@
+
+public class constantes {
+
+	public static void main(String[] args) {
+		
+		// variable----------------------------------------
+		int numero = 10;
+		numero=15;
+		System.out.println("el numero variable es "+numero);
+		
+		// constante---------------------------------------
+		final int numer = 10;
+		System.out.println("el numero constante es "+numer);
+		 
+	}
+
+}
